866fc5280481625c84d5b6bb0cf8c601414f5fa8
William <willspri@gmail.com>
2024-05-04 13:01:38 +1200

    added save to b64.txt

--- hexToBase64.ps1
+++ hexToBase64.ps1
@@ -6,5 +6,7 @@ $fileBytes = Get-Content -Path $file -Encoding Byte
 $hexDump = $fileBytes | ForEach-Object { $_.ToString("X2") }
 # Convert the hexadecimal data to base64
 $base64Data = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($hexDump))
+$base64Data | Set-Content -Path "b64.txt" -Encoding Unicode
+
 # Output the base64 data
 $base64Data
